7b6dcf8dbd3b8b7867c06a7f6e4dc8d651dbb173
dohyuk <didehgur1@naver.com>
2019-04-08 08:47:46 +0000

    190408 | Django insta

---
+++ django/insta/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name = 'posts'
+
+urlpatterns = [
+    path('create/',views.create, name = 'create'),
+    path('',views.list, name = 'list'),
+    path('<int:post_id>/delete/',views.delete, name='delete'),
+]
