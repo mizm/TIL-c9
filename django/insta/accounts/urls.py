3635ee6cb911109f9c2c287062058ddb195cda6d
dohyuk <didehgur1@naver.com>
2019-04-11 08:46:27 +0000

    190411 | comment,like,people

--- django/insta/accounts/urls.py
+++ django/insta/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from accounts import views as accounts_views
 
 app_name = 'accounts'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
+    path('<str:username>/',accounts_views.people, name='people'),
 ]
--- django/insta/posts/models.py
+++ django/insta/posts/models.py
@@ -18,3 +18,9 @@ class Post(models.Model) :
             format='JPEG',#포맷 저장
             options={'quality':90},#옵션
         )
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
+class Comment(models.Model) :
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete =models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    
